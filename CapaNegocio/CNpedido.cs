48b82086f55cf90e24b11486d4408bfc29ce2240
tatianazapata88 <70298165+tatianazapata88@users.noreply.github.com>
2021-01-18 11:10:32 -0500

    capa negocio

--- CapaNegocio/CNpedido.cs
+++ CapaNegocio/CNpedido.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CapaDato;
+using CapaEntidad;
+using System.Data;
+
 
 namespace CapaNegocio
 {
     public class CNpedido
     {
+        CDpedido odatpedido = new CDpedido();
+
+        public bool crear_pedido(CEpedido oentpedido)
+        {
+            return odatpedido.guardar(oentpedido);
+        }
+
+        public bool eliminar_pedido(CEpedido oentpedido)
+        {
+            return odatpedido.eliminarpedido(oentpedido);
+        }
+
+        public DataSet consultar_pedido(CEpedido oentpedido)
+        {
+            return odatpedido.listarPedidos(oentpedido);
+        }
+
+
     }
 }
